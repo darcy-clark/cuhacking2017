dc410b7de2b3df47c547c48a049358f1dd214ceb
andrewpai148@Hotmail.com <rubycpai4>
2017-03-05 10:58:36 -0500

    Fixed Darcy's script problem for overview

--- resources/Scripts/load_notification.js
+++ resources/Scripts/load_notification.js
@@ -2,12 +2,20 @@
  * Created by wilsontsang on 2017-03-05.
  */
 
-var deviceCount;
-var listNotifications = new Array()
-$(document).ready(function () {
-    getdeviceCount()
-});
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        };
 
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send( null );
+    }
+};
+
+var deviceCount;
 
 function displayAlerts(device) {
 
@@ -16,26 +24,9 @@ function displayAlerts(device) {
             url: "http://cuhackathon-challenge.martellotech.com/devices/" + device
         }).then(function (data) {
 
-            var device = data.name;
-           for(var i = 0; i< data.alarms.length;i++) {
-               console.log(data.alarms)
-                if(data.alarms[0].message){
-                    var alm = data.alarms[0].message
-                }
-                if(data.alarms[0].description ){
-                    var alm = data.alarms.description
-                }
-           }
-
-            if (device !== "HomeGateway" && device !== "Family-Desktop"){
-                var notification = {device: device, alm: alm};
-                listNotifications.push(notification);
-
-                $(notification_list).prepend(" <li class='list-group-item'>" + device + "\r" +
-                    alm + "</li>"
-                );
-            }
-
+            console.log(data.name)
+            $(notification_list).append(" <li class='list-group-item'>" + data.dexscription + "</li>");
+            $(device_list).append(" <li class='list-group-item'>" + data.name + "</li>");
         });
     });
 }
@@ -55,13 +46,6 @@ function getdeviceCount() {
     });
 }
 
-function search(nameKey, myArray){
-    for (var i=0; i < myArray.length; i++) {
-        if (myArray[i].name === nameKey) {
-            console.log(myArray[i].name + " " + nameKey)
-            return true;
-        }
-    }
-}
+//setInterval(getdeviceCount, 1000);
 
-
+getdeviceCount();
---
+++ resources/Scripts/load_overview.js
@@ -0,0 +1,71 @@
+/**
+ * Created by andre on 2017-03-05.
+ */
+/**
+ * Created by wilsontsang on 2017-03-05.
+ */
+
+var deviceCount;
+var listNotifications = new Array()
+$(document).ready(function () {
+    getdeviceCount()
+});
+
+
+function displayAlerts(device) {
+
+    $(document).ready(function () {
+        $.ajax({
+            url: "http://cuhackathon-challenge.martellotech.com/devices/" + device
+        }).then(function (data) {
+
+            var device = data.name;
+            $(device_list).prepend(" <li class='list-group-item'>" + device + "</li>")
+            for(var i = 0; i< data.alarms.length;i++) {
+                console.log(data.alarms)
+                if(data.alarms[0].message){
+                    var alm = data.alarms[0].message
+                }
+                if(data.alarms[0].description ){
+                    var alm = data.alarms.description
+                }
+            }
+
+            if (device !== "HomeGateway" && device !== "Family-Desktop"){
+                var notification = {device: device, alm: alm};
+                listNotifications.push(notification);
+
+                $(notification_list).prepend(" <li class='list-group-item'>" + device + "\r" +
+                    alm + "</li>"
+                );
+            }
+
+        });
+    });
+}
+
+function getdeviceCount() {
+    var client = new HttpClient();
+    client.get('http://cuhackathon-challenge.martellotech.com/devices', function(response) {
+        // do something with response
+        var response_json = JSON.parse(response);
+        deviceCount =Object.keys(response_json).length
+        console.log (deviceCount);
+
+        for(var count = 0; count < deviceCount; count++) {
+            displayAlerts(count)
+        }
+
+    });
+}
+
+function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].name === nameKey) {
+            console.log(myArray[i].name + " " + nameKey)
+            return true;
+        }
+    }
+}
+
+
