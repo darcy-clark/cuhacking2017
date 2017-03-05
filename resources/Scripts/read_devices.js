92b846b5c818d27bf692a95de916a0d5fec4007f
Darcy Clark <dgrclark@edu.uwaterloo.ca>
2017-03-04 21:54:53 -0500

    reads JSON properly for devices

--- resources/Scripts/read_devices.js
+++ resources/Scripts/read_devices.js
@@ -1,2 +1,20 @@
 
-var myJSON = make_request("devices/1");
+var inJSON;
+var deviceArray = [];
+var firstJSON = true;
+var numDevices;
+
+function createJSON(myJSON) {
+    inJSON = myJSON;
+    numDevices = Object.keys(inJSON).length;
+    console.log('Num Devices: ' + numDevices);
+    for(var count = 1; count < numDevices; count++) {
+        make_request('devices/' + count);
+    }
+}
+
+function createDevices(myJSON) {
+    deviceArray.push(myJSON);
+}
+
+make_request_all();
